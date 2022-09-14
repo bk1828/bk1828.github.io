9d30018b2f52b1cead5db592c3689741c8bb29f5
bk <cbk1828@gmail.com>
2022-09-14 15:26:06 +0900

    mvc paging

--- SpringMVC/src/main/java/first/sample/service/SampleServiceImpl.java
+++ SpringMVC/src/main/java/first/sample/service/SampleServiceImpl.java
@@ -16,23 +16,22 @@ import first.sample.dao.SampleDAO;
 @Service("sampleService")
 public class SampleServiceImpl implements SampleService{
 	Logger log = Logger.getLogger(this.getClass());
-
+	
 	@Resource(name="fileUtils")
 	private FileUtils fileUtils;
-
+	
 	@Resource(name="sampleDAO")
 	private SampleDAO sampleDAO;
-
+	
 	@Override
-	public List<Map<String, Object>> selectBoardList(Map<String, Object> map) throws Exception {
+	public Map<String, Object> selectBoardList(Map<String, Object> map) throws Exception {
 		return sampleDAO.selectBoardList(map);
-
 	}
 
 	@Override
 	public void insertBoard(Map<String, Object> map, HttpServletRequest request) throws Exception {
 		sampleDAO.insertBoard(map);
-
+		
 		List<Map<String,Object>> list = fileUtils.parseInsertFileInfo(map, request);
 		for(int i=0, size=list.size(); i<size; i++){
 			sampleDAO.insertFile(list.get(i));
@@ -45,17 +44,17 @@ public class SampleServiceImpl implements SampleService{
 		Map<String, Object> resultMap = new HashMap<String,Object>();
 		Map<String, Object> tempMap = sampleDAO.selectBoardDetail(map);
 		resultMap.put("map", tempMap);
-
+		
 		List<Map<String,Object>> list = sampleDAO.selectFileList(map);
 		resultMap.put("list", list);
-
+		
 		return resultMap;
 	}
 
 	@Override
 	public void updateBoard(Map<String, Object> map, HttpServletRequest request) throws Exception{
 		sampleDAO.updateBoard(map);
-
+		
 		sampleDAO.deleteFileList(map);
 		List<Map<String,Object>> list = fileUtils.parseUpdateFileInfo(map, request);
 		Map<String,Object> tempMap = null;
