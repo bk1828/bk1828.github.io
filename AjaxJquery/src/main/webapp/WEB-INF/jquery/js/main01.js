02496a8a763e423cd15d09cfc5947f7708782253
bk <cbk1828@gmail.com>
2022-09-15 16:52:51 +0900

    jquery 연습

--- AjaxJquery/src/main/webapp/WEB-INF/jquery/js/main01.js
+++ AjaxJquery/src/main/webapp/WEB-INF/jquery/js/main01.js
@@ -15,13 +15,20 @@
 		//h1의 css 색깔을 red로
 		$('h1').css({'color':'red'});
 		
-		
+		/*
 		$('#typo .inner').css({
 			textDecoration:'underline',   //#typo. inner의 글자를 장식. 밑줄긋기
 			borderBottom:'5px solid red', //#typo. inner의 테두리. 5px 빨간줄
 			transform:'rotate(45deg)',    //#typo. inner의 회전(45도)
 			opacity:0.5  				  //#typo. inner의 투명도 설정(0.5)
 		});
-		
+		*/
+			
+		$('#typo').mouseover(function(){
+			$(this).css('background-color','green');
+		})
+		.mouseout(function(){
+			$(this).css('background-color','#3498db');
+		});
 	});	
 	
